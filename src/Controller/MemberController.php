742eda8467c808d4a47562819e9b08b4512ad643
Filipe Carvalhais Sanches <filipe78190@gmail.com>
2016-10-26 14:56:33 +0200

    création des fighters
    ajout de la page addfighters
    Modification de la page index.ctp des membre
    créaion fightersTables

--- src/Controller/MemberController.php
+++ src/Controller/MemberController.php
@@ -12,16 +12,37 @@ class MemberController extends AppController
         parent::beforeFilter($event);
         //$this->Auth->allow(['index']);
         $this->viewBuilder()->layout("member");
-        $this->set('authUser', $this->Auth->user());
+				$user = $this->Auth->user();
+        $this->set('authUser', $user);
     }
 
     public function index(){
-    	
+
     }
+
+		public function addfighters(){
+			$user = $this->Auth->user();
+			$this->loadModel('Fighters');
+      $fighter = $this->Fighters->newEntity();
+      if ($this->request->is('post'))
+        {
+          $fighter = $this->Fighters->patchEntity($fighter, $this->request->data);
+					$value = $this->Fighters->insert($fighter,$user);
+					if ($value)
+		        {
+		          $this->Flash->success(__("Le combattant a été ajouté."));
+		          return $this->redirect(['action' => 'index']);
+		        }
+					else
+					 	{
+						$this->Flash->error(__("Impossible d'ajouter le combattant."));
+						}
+        }
+		}
 
     public function deconnexion(){
     	$this->Flash->success('Vous êtes maintenant déconnecté.');
-        return $this->redirect($this->Auth->logout()); 
+        return $this->redirect($this->Auth->logout());
     }
 
 }
